226810249f724b4ba11364e0cf835ed9861ddbbd
Filipe da Silva Granja <110956203+filipegranja98@users.noreply.github.com>
2023-11-07 19:18:29 -0300

    Update UsuarioColaborador.java

--- src/UsuarioColaborador.java
+++ src/UsuarioColaborador.java
@@ -2,12 +2,11 @@ import java.util.ArrayList;
 
 public class UsuarioColaborador extends Usuario{
     private String cpf;
-    private ArrayList<Ebook> ebooksEscritos;
+   
 
     public UsuarioColaborador(String cpf,String nome, String email,String endereco,String numeroContato) {
         super(nome,email,endereco,numeroContato);
         this.cpf = cpf;
-        this.ebooksEscritos = new ArrayList<>();
     }
 
     public String getCpf() {
@@ -16,14 +15,6 @@ public class UsuarioColaborador extends Usuario{
 
     public void setCpf(String cpf) {
         this.cpf = cpf;
-    }
-
-    public ArrayList<Ebook> getEbooksEscritos() {
-        return ebooksEscritos;
-    }
-
-    public void setEbooksEscritos(ArrayList<Ebook> ebooksEscritos) {
-        this.ebooksEscritos = ebooksEscritos;
     }
 
     public void adicionarEbook(Ebook ebook) {
